e59cbf737e0e188297e130e15b881cd128d5964f
Samuel <ntat.ecd@email.com>
2025-08-21 14:34:39 +0700

    feat: Redirect to /login if token is expired.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { useNavigate } from "react-router";
 import LoginPage from "./components/LoginPage";
 import AdminPage from "./components/AdminPage";
@@ -15,12 +15,6 @@ function App() {
   const [authToken, setAuthToken] = useState(
     localStorage.getItem("authToken") || null
   );
-  useEffect(() => {
-    const token = localStorage.getItem("authToken");
-    if (token) {
-      setAuthToken(token);
-    }
-  }, []);
 
   const AppContent = () => {
     const navigate = useNavigate();
@@ -31,9 +25,38 @@ function App() {
       navigate("/login");
     };
 
+    useEffect(() => {
+      const token = localStorage.getItem("authToken");
+      if (token) {
+        const verifyToken = async () => {
+          try {
+            const response = await fetch(
+              "http://localhost:3000/api/auth/verify",
+              {
+                headers: {
+                  "x-auth-token": token,
+                },
+              }
+            );
+            //If not valid
+            if (!response.ok) {
+              throw new Error("Token verification failed");
+            }
+            //If valid, set the auth token
+            setAuthToken(token);
+          } catch (error) {
+            console.error("Token verification failed:", error);
+            handleLogout();
+          }
+        };
+
+        verifyToken();
+      }
+    }, []);
+
     return (
       <div className="bg-gray-100 min-h-screen">
-        <Header authToken={authToken} onLogout={handleLogout}/>
+        <Header authToken={authToken} onLogout={handleLogout} />
         <Routes>
           <Route path="/" element={<EventsPage />} />
           <Route
